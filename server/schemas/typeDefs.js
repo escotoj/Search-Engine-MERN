2a8227bc67b44ee25cb6809dad8ea8917216a1a2
escotoj <j.escoto14@gmail.com>
2023-06-29 13:16:41 -0700

    add notations and structure to thr typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,52 @@
 // similar to models where we call gql from apollo
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-// must match the models 
+// must match the models
+// QUERYs are GET
+// MUTATIONs are POST PUT DELETE
+// the ! means its required
+
+// 4 mutations are involved  
+// login can be seen as a combination of POST and GET.
+// addUser is similar to a POST request.
+// saveBook can be seen as a combination of PUT and POST.
+// removeBook is similar to a DELETE request.
+
+const typeDefs = gql`
+type Query {
+me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput!): User
+    removeBook(bookId: String!): User
+}
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token: String
+    user: User
+  }
+  input SaveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String`;
+
+module.exports = typeDefs;
